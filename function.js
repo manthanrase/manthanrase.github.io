dcb75aa2a63ae9b6c0555a09d6fdef482fba249b
Manthan Rase <manthanrase2211@gmail.com>
2023-06-21 22:04:53 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggleBtnIcon = document.querySelector('.toggle-btn i');
+const primaryNav = document.querySelector('.dropdown-open');
+
+
+toggleBtnIcon.addEventListener('click', function(){
+  const visibility = primaryNav.getAttribute('data-visible')
+  if (visibility==='false'){
+    primaryNav.setAttribute('data-visible', true)
+  } else {
+    primaryNav.setAttribute('data-visible', false)
+  }
+})
